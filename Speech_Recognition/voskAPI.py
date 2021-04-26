8128b8ff12ba71c1ba15803cd91cd76c1821c692
Lucas-Nardi <lucas.2017107156@unicp.br>
2021-04-26 16:24:52 -0300

    change play_button_icon when finish recognition added

--- Speech_Recognition/voskAPI.py
+++ Speech_Recognition/voskAPI.py
@@ -1,3 +1,4 @@
+from PyQt5 import QtGui, QtCore
 from vosk import Model, KaldiRecognizer
 import threading
 import os
@@ -6,20 +7,23 @@ import pyaudio
 from pynput.keyboard import Key, Controller
 import time
 
-
 if (not os.path.exists("Speech_Recognition/Model/Portugues/model") ):
     print ("Please download the model-long-small from https://alphacephei.com/vosk/models and unpack as 'model-long-small' in the current folder.")
     exit (1)
 
-class Speech_Recognition:
+class Recognition:
 
     pressed_keys_list = list()
     commands_list_dicionary = dict()
     keyboard = Controller()
+    p = pyaudio.PyAudio()
+    stream = None
+    stop_recognition = None
 
-    def voice_commands(self,commands_file_name):
+    def voice_commands(self,commands_file_name,buttom_component,menu_class ):
 
-        file_path = "../Games/" + commands_file_name
+        self.stop_recognition = False
+        file_path = "Games/" + commands_file_name
 
         recognized_commands = ""
 
@@ -49,154 +53,161 @@ class Speech_Recognition:
                         recognized_commands = recognized_commands + command2 + " "
                         self.commands_list_dicionary.__setitem__(command2,[key,need_to_press[0]])
 
+
             commands_file.close()
-            recognized_commands = recognized_commands + "pare finalizar programa"
+            recognized_commands = recognized_commands + "pare programa finalizar"
+            model = Model("Speech_Recognition/Model/Portugues/model")
+            self.stream = self.p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
+            self.stream.start_stream()
+            speech = ""
+            prev_speech = "oioi"
+            new_comand = 0
+            recognized_commands = "\"erro " +recognized_commands + " fruta\""
 
-        model = Model("./Model/Portugues/model")
+            rec = KaldiRecognizer(model, 16000,recognized_commands)
 
-        p = pyaudio.PyAudio()
-        stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
-        stream.start_stream()
-        speech = ""
-        prev_speech = ""
-        new_comand = 0
-        print(recognized_commands)
+            while (not self.stop_recognition):
 
-        rec = KaldiRecognizer(model, 16000,recognized_commands)
-        while True:
-
-            data = stream.read(4000,exception_on_overflow = False)
-            if len(data) == 0:
-                break
-            if  (rec.AcceptWaveform(data)):
-                final = rec.Result()
-                new_comand = 0
-                speech = " "
-            else:
-                partial = rec.PartialResult()
-                result = json.loads(partial)
-                speech = result["partial"]
-
-                phrase = speech.split(" ")
-                speech = phrase[len(phrase) - 1]
-
-
-                if (new_comand == len(phrase) and new_comand != 0):
+                data = self.stream.read(4000,exception_on_overflow = False)
+                if len(data) == 0:
+                    break
+                if  (rec.AcceptWaveform(data)):
+                    final = rec.Result()
+                    new_comand = 0
                     speech = " "
+                else:
+                    partial = rec.PartialResult()
+                    result = json.loads(partial)
+                    speech = result["partial"]
 
-                if (speech in self.commands_list_dicionary):
-                    command = self.commands_list_dicionary[speech]
-                    key = command[0]
-                    press_key = command[1]
+                    phrase = speech.split(" ")
+                    speech = phrase[len(phrase) - 1]
 
-                    if(key == "space"):
-                        if(press_key == "yes"):              # Preciso manter precionada
-                            self.keyboard.press(Key.space)
-                            self.pressed_keys_list.append(key)
-                        else:
-                            self.keyboard.press(Key.space)
-                            time.sleep(0.500)
-                            self.keyboard.release(Key.space)
-                    elif(key == "shift"):
-                        if (press_key == "yes"):
-                            self.keyboard.press(Key.shift)
-                            self.pressed_keys_list.append(key)
-                        else:
-                            self.keyboard.press(Key.shift)
-                            time.sleep(0.500)
-                            self.keyboard.release(Key.shift)
-                    elif(key == "ctrl"):
-                        if (press_key == "yes"):
-                            self.keyboard.press(Key.ctrl)
-                            self.pressed_keys_list.append(key)
-                        else:
-                            self.keyboard.press(Key.ctrl)
-                            time.sleep(0.500)
-                            self.keyboard.release(Key.ctrl)
-                    elif(key == "tab"):
-                        if (press_key == "yes"):
-                            self.keyboard.press(Key.tab)
-                            self.pressed_keys_list.append(key)
-                        else:
-                            self.keyboard.press(Key.tab)
-                            time.sleep(0.500)
-                            self.keyboard.release(Key.tab)
+                    print(speech)
 
-                    else: # Not special commands
-                        if (press_key == "yes"):
+                    if (new_comand == len(phrase) and new_comand != 0):
+                        speech = " "
 
-                            if(key == "a"):
+                    if (speech in self.commands_list_dicionary):
+                        command = self.commands_list_dicionary[speech]
+                        key = command[0]
+                        press_key = command[1]
 
-                                if("d" in self.pressed_keys_list):
-                                    print("Liberando d")
-                                    self.keyboard.release("d")
-                                    self.pressed_keys_list.remove("d")
+                        if(key == "space"):
+                            if(press_key == "yes"):              # Preciso manter precionada
+                                self.keyboard.press(Key.space)
+                                self.pressed_keys_list.append(key)
+                            else:
+                                self.keyboard.press(Key.space)
+                                time.sleep(0.500)
+                                self.keyboard.release(Key.space)
+                        elif(key == "shift"):
+                            if (press_key == "yes"):
+                                self.keyboard.press(Key.shift)
+                                self.self.pressed_keys_list.append(key)
+                            else:
+                                self.keyboard.press(Key.shift)
+                                time.sleep(0.500)
+                                self.keyboard.release(Key.shift)
+                        elif(key == "ctrl"):
+                            if (press_key == "yes"):
+                                self.keyboard.press(Key.ctrl)
+                                self.pressed_keys_list.append(key)
+                            else:
+                                self.keyboard.press(Key.ctrl)
+                                time.sleep(0.500)
+                                self.keyboard.release(Key.ctrl)
+                        elif(key == "tab"):
+                            if (press_key == "yes"):
+                                self.keyboard.press(Key.tab)
+                                self.pressed_keys_list.append(key)
+                            else:
+                                self.keyboard.press(Key.tab)
+                                time.sleep(0.500)
+                                self.keyboard.release(Key.tab)
 
-                            if (key == "d"):
+                        else: # Not special commands
+                            if (press_key == "yes"):
 
-                                if ("a" in self.pressed_keys_list):
-                                    print("Liberando a")
-                                    self.keyboard.release("a")
-                                    self.pressed_keys_list.remove("a")
+                                if(key == "a"):
 
-                            if (key == "w"):
+                                    if("d" in self.pressed_keys_list):
+                                        print("Liberando d")
+                                        self.keyboard.release("d")
+                                        self.pressed_keys_list.remove("d")
 
-                                if ("s" in self.pressed_keys_list):
-                                    print("Liberando s")
-                                    self.keyboard.release("s")
-                                    self.pressed_keys_list.remove("s")
+                                if (key == "d"):
 
-                            if (key == "s"):
+                                    if ("a" in self.pressed_keys_list):
+                                        print("Liberando a")
+                                        self.keyboard.release("a")
+                                        self.pressed_keys_list.remove("a")
 
-                                if ("w" in self.pressed_keys_list):
-                                    print("Liberando w")
-                                    self.keyboard.release("w")
-                                    self.pressed_keys_list.remove("w")
+                                if (key == "w"):
 
-                            self.keyboard.press(key)
-                            self.pressed_keys_list.append(key)
-                        else:
-                            self.keyboard.press(key)
-                            time.sleep(0.500)
-                            self.keyboard.release(key)
+                                    if ("s" in self.pressed_keys_list):
+                                        print("Liberando s")
+                                        self.keyboard.release("s")
+                                        self.pressed_keys_list.remove("s")
 
-                    new_comand = len(phrase)
-                    prev_speech = speech
+                                if (key == "s"):
 
-                elif(speech == "pare"):  # Release all keys that was pressed
+                                    if ("w" in self.pressed_keys_list):
+                                        print("Liberando w")
+                                        self.keyboard.release("w")
+                                        self.pressed_keys_list.remove("w")
 
-                    for key in self.pressed_keys_list:
+                                self.keyboard.press(key)
+                                self.pressed_keys_list.append(key)
+                            else:
+                                self.keyboard.press(key)
+                                time.sleep(0.500)
+                                self.keyboard.release(key)
 
-                        if (key == "space"):
-                            self.keyboard.release(Key.space)
-                        elif (key == "shift"):
-                            self.keyboard.release(Key.shift)
-                        elif (key == "ctrl"):
-                            self.keyboard.release(Key.ctrl)
-                        elif (key == "tab"):
-                            self.keyboard.release(Key.tab)
-                        else:
-                            self.keyboard.release(key)
+                        new_comand = len(phrase)
+                        prev_speech = speech
 
-                    new_comand = len(phrase)
-                    prev_speech = speech
+                    elif(speech == "pare"):  # Release all keys that was pressed
 
-                    self.pressed_keys_list.clear()
+                        for key in self.pressed_keys_list:
 
-                if(speech == "finalizar"):
-                    prev_speech = "finalizar"
+                            if (key == "space"):
+                                self.keyboard.release(Key.space)
+                            elif (key == "shift"):
+                                self.keyboard.release(Key.shift)
+                            elif (key == "ctrl"):
+                                self.keyboard.release(Key.ctrl)
+                            elif (key == "tab"):
+                                self.keyboard.release(Key.tab)
+                            else:
+                                self.keyboard.release(key)
 
-                if(prev_speech == "finalizar" and speech == "programa"):
-                    print("Indo finalizar pograma")
-                    break;
+                        new_comand = len(phrase)
+                        prev_speech = speech
+
+                        self.pressed_keys_list.clear()
+
+                    if(speech == "finalizar"):
+                        prev_speech = "finalizar"
+
+                    if(prev_speech == "finalizar" and speech == "programa"):
+                        self.stop_recognition = True
+
+            self.exit(buttom_component,menu_class)
+
+    def exit(self,buttom_component,menu_class):
+
+        play_icon = QtGui.QIcon()
+        play_icon.addPixmap(QtGui.QPixmap("Images/play_button.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        buttom_component.setIcon(play_icon)
+        buttom_component.setIconSize(QtCore.QSize(50, 50))
+        menu_class.playing = False
+        self.stream.stop_stream()
+        self.stream.close()
 
 
-        print("Finalizando Programa")
-        stream.stop_stream()
-        stream.close()
 
-
-
+# voice_commands("Fall Guy's.csv")
 
 # threading.Thread(target=speech.voice_commands("Fall Guy's.csv")).start()
 
